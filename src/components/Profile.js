84756caeed6d4b84c7215a0b5dbadd72e0669006
Nitu Dubey <nitutiwari01@gmail.com>
2023-07-29 09:26:01 -0400

    change in css file

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,17 +3,23 @@ import { connect } from "react-redux";
 
 const Profile = ({ user }) => {
   return (
+    <div><h1>Profile</h1>
     <div className="profile-page">
+    
       <div className="profile">
-        <img src={user.image} alt="profile_image" />
+      <div className="image">
+      <img src={user.image} alt="profile_image" />
+      </div>
+        
         <div className="user-info">
-          <div className="fname">First Name: {user.firstName}</div>
-          <div className="lname">Last Name: {user.lastName}</div>
-          <div className="email">Email: {user.email}</div>
-          <div className="username">Username: {user.username}</div>
-          <div>Gender: male</div>
+          <div className="fname"><b>First Name:</b> {user.firstName}</div>
+          <div className="lname"><b>Last Name:</b> {user.lastName}</div>
+          <div className="email"><b>Email:</b> {user.email}</div>
+          <div className="username"><b>Username:</b> {user.username}</div>
+          <div><b>Gender:</b> {user.gender}</div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
